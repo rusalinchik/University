3a9f3bc8f19052d6df2a36ad99c9c4150b4e0155
verenahu <rusalinchikk@yandex.ru>
2025-09-12 19:48:30 +0300

    2

---
+++ Информатика/Лабораторная работа №1/task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    double speed;
+    std::cout << "Введите скорость автомобиля (км/ч):";
+    std::cin >> speed;
+    if (speed < 0)
+        std::cout << "Скорость не может быть отрицательной" << std::endl;
+    if (speed < 60) 
+        std::cout << "Скорость в пределах нормы" << std::endl;
+    else if (speed <= 80) 
+        std::cout << "Вы приближаетесь к привышению скорости" << std::endl;
+    else 
+        std::cout << "Превышение скорости" << std::endl;
+    return 0;
+}
---
+++ Информатика/Лабораторная работа №1/task2/task2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	int car;
+	unsigned long long p = 1;
+	int counter = 1;
+	std::cout << "Введите кол-во автомобилей, произведённых на фабрике:";
+	std::cin >> car;
+	if (car < 0) {
+		std::cout << "Количество машин не может быть отрицательное!";
+		return 1;
+	}
+	while (counter <= car) {
+		p *= counter;
+		counter++;
+	}
+	std::cout << "Производительность машин на фабрике: " << p;
+}
