585d4e13dc13e168d6ffe556d8a25355c2e7c9b4
verenahu <rusalinchikk@yandex.ru>
2025-10-19 19:06:02 +0300

    3

---
+++ ОАиП/Семинар 6. Задачи/task3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstring>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int SIZE = 256;
+    char str[SIZE];
+
+    std::cout << "Введите строку: ";
+    std::cin.getline(str, SIZE);
+
+    int len = std::strlen(str);
+    int j = 0; 
+
+    for (int i = 0; i < len; i++) {
+        if (!(str[i] >= '0' && str[i] <= '9')) { 
+            str[j] = str[i];
+            j++;
+        }
+    }
+
+    str[j] = '\0'; 
+
+    std::cout << "Результат: " << str << std::endl;
+
+    return 0;
+}
